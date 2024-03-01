e39e0c90211bf48df216c955e551de2a5a6067d6
Asar_kun <asarkrylov76212@gmail.com>
2024-03-01 17:07:52 +0300

    Main structure for start of bot is done

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from dotenv import load_dotenv
+
+from bot.handlers.user_handlers import reg_handler
+
+
+async def main() -> None:
+    
+    load_dotenv(".env")
+
+    storage = MemoryStorage()
+
+    token = os.getenv("TOKEN")
+    bot = Bot(token)
+    dp = Dispatcher(bot, storage=storage)
+    
+    reg_handler(dp)
+
+    try:
+        await dp.start_polling(bot)
+    except:
+        await bot.session.close()
+        await bot.close_bot()
+        print("Bot is stop!")
 
 
 if __name__ == "__main__":
-    pass
+    asyncio.run(main())
