f2b3c48c3dae0b469de86a96ead46ce1b6d5de9b
Asar_kun <asarkrylov76212@gmail.com>
2024-03-04 18:33:16 +0300

    Add new state for enter class

--- bot/states/user_states.py
+++ bot/states/user_states.py
@@ -7,3 +7,4 @@ class States(StatesGroup):
     # main_menu = State("main_menu")
     # view_classes = State("view_classes")
     waiting_enter_login = State("waiting_enter_login")
+    waiting_enter_class = State("waiting_enter_class")
