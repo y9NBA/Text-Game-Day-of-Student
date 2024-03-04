368f2406b1f70a5b8f6a589fc237123bc8bef2d5
Asar_kun <asarkrylov76212@gmail.com>
2024-03-04 18:34:35 +0300

    Add kb for view_classe and enter_class

--- bot/keyboards/user_kb.py
+++ bot/keyboards/user_kb.py
@@ -3,15 +3,16 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, R
 from DataBase.models import dict_classes
 
 
-callbacks = ["user_login", "profile", "view_classes"]
-callbacks_back = ["user_login_b", "profile_b", "view_classes_b"]
+callbacks = ["user_login", "profile", "classes"]
+callbacks_back = ["main_menu"]
+
+classes = [x for x in dict_classes]
 
 
 def get_start_kb() -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-    [
-        InlineKeyboardButton(text="Логин", callback_data=callbacks[0])
-    ]
+        [InlineKeyboardButton(text="Логин", callback_data=callbacks[0])],
+        [InlineKeyboardButton(text="Специальности", callback_data=callbacks[2])]
     ])
     return keyboard
 
@@ -26,9 +27,26 @@ def get_login_kb() -> InlineKeyboardMarkup:
 
 
 def get_view_classes_kb() -> InlineKeyboardMarkup:
-    classes_kb = [InlineKeyboardButton(text=f"{x}") for x in dict_classes.keys()]
+    classes_kb = [
+            [
+                InlineKeyboardButton(text=classes[i], callback_data=classes[i]),
+                InlineKeyboardButton(text=classes[i+1], callback_data=classes[i+1])
+            ]
+            for i in range(0, len(classes), 2)
+            if i+1 < len(classes)
+    ]
+    classes_kb.append([InlineKeyboardButton(text=classes[-1], callback_data=classes[-1])])
+    classes_kb.append([InlineKeyboardButton(text="< Назад", callback_data=callbacks_back[0])])
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=classes_kb
+    )
+    return keyboard
+
+
+def get_enter_class_kb(class_name: str) -> InlineKeyboardMarkup:
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
-        classes_kb.append(InlineKeyboardButton(text="< Назад", callback_data=callbacks_back[2]))
+        [InlineKeyboardButton(text='Выбрать класс', callback_data=class_name)],
+        [InlineKeyboardButton(text="< Назад", callback_data=callbacks[2])]
     ])
     return keyboard
 
